c1c05432392c2150853dc470300e7179b2428276
Zakheer Ali <159714713+ZakheerAli@users.noreply.github.com>
2025-04-26 16:08:34 +0500

    Update counter.py

--- Assignment 1 to 6/counter.py
+++ Assignment 1 to 6/counter.py
@@ -1,3 +1,19 @@
+
+import time
+try:
+    user_time=int(input("Enter time in second: "))
+    for i in range(user_time,0,-1):
+        hours=int( i / 3600)
+        mins=int(( i / 60 ) % 60)
+        sec=i % 60
+        print(f"\r{hours:02}:{mins:02}:{sec:02}",end="" , flush=True)
+        time.sleep(1)
+    print("time end")
+except ValueError:
+    print("Invalid Input")
+ 
+
+
 # import time
 
 # user_time=int(input("Enter the time in seconds: "))
@@ -28,18 +44,5 @@
 # print("Time's Up")
 
 
-import time
-try:
-    user_time=int(input("Enter time in second: "))
-    for i in range(user_time,0,-1):
-        hours=int( i / 3600)
-        mins=int(( i / 60 ) % 60)
-        sec=i % 60
-        print(f"\r{hours:02}:{mins:02}:{sec:02}",end="" , flush=True)
-        time.sleep(1)
-    print("time end")
-except ValueError:
-    print("Invalid Input")
- 
 
 
